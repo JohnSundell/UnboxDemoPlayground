809dea0cf99a78677b24dbbd0c0334eb36af70ef
John Sundell <john.sundell@me.com>
2016-05-02 21:32:19 +0200

    Add Playground + Unbox & Wrap submodules

---
+++ UnboxDemo.playground/Contents.swift
@@ -0,0 +1,52 @@
+import Foundation
+import Unbox
+import Wrap
+
+enum Profession: String, UnboxableEnum {
+    case Developer
+    case Lawyer
+    
+    static func unboxFallbackValue() -> Profession {
+        return .Lawyer
+    }
+}
+
+struct Device: Unboxable {
+    let name: String
+    let inchSize: Double
+    
+    init(unboxer: Unboxer) {
+        self.name = unboxer.unbox("name")
+        self.inchSize = unboxer.unbox("inchSize")
+    }
+}
+
+struct User: Unboxable {
+    let name: String
+    let age: Int
+    let profession: Profession
+    let devices: [Device]
+    
+    init(unboxer: Unboxer) {
+        self.name = unboxer.unbox("name")
+        self.age = unboxer.unbox("age")
+        self.profession = unboxer.unbox("profession")
+        self.devices = unboxer.unbox("devices")
+    }
+}
+
+// Load JSON data
+let bundle = NSBundle.mainBundle()
+let path = bundle.pathForResource("B", ofType: ".json")!
+let data = NSData(contentsOfFile: path)!
+
+// Perform unboxing
+do {
+    let user: User = try Unbox(data)
+    print(user)
+    
+    let userData: NSData = try Wrap(user)
+    let user2: User = try Unbox(userData)
+} catch {
+    print(error)
+}
